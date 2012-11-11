8c019a7147c45f8c35dd8551a4f13cdf9cffda36
Shuji Watanabe <shuji.w6e@gmail.com>
2012-11-11 10:31:16 +0900

    Enclosedテストランナーによるテストコードの構造化を適用

--- src/test/java/junitbook/ArrayListTest.java
+++ src/test/java/junitbook/ArrayListTest.java
@@ -5,57 +5,83 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class ArrayListTest {
 
-    @Test
-    public void リストが初期状態の時sizeは0を返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        assertThat(sut.size(), is(0));
+    public static class リストが初期状態の時 {
+
+        private ArrayList<String> sut;
+
+        @Before
+        public void setUp() throws Exception {
+            sut = new ArrayList<>();
+        }
+
+        @Test
+        public void sizeは0を返す() throws Exception {
+            assertThat(sut.size(), is(0));
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void get_0はIndexOutOfBoundsExceptionを送出する() throws Exception {
+            sut.get(0);
+        }
+
     }
 
-    @Test
-    public void リストに1件の文字列を含む時sizeは1を返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.add("hello");
-        assertThat(sut.size(), is(1));
+    public static class リストに1件の文字列を含む時 {
+
+        private ArrayList<String> sut;
+
+        @Before
+        public void setUp() throws Exception {
+            sut = new ArrayList<>();
+            sut.add("hello");
+
+        }
+
+        @Test
+        public void sizeは1を返す() throws Exception {
+            assertThat(sut.size(), is(1));
+        }
+
+        @Test
+        public void get_0はhelloを返す() throws Exception {
+            assertThat(sut.get(0), is("hello"));
+        }
+
     }
 
-    @Test
-    public void リストに2件の文字列を含む時sizeは2を返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.add("hello");
-        sut.add("world");
-        assertThat(sut.size(), is(2));
+    public static class リストに2件の文字列を含む時 {
+
+        private ArrayList<String> sut;
+
+        @Before
+        public void setUp() throws Exception {
+            sut = new ArrayList<>();
+            sut.add("hello");
+            sut.add("world");
+        }
+
+        @Test
+        public void sizeは2を返す() throws Exception {
+            assertThat(sut.size(), is(2));
+        }
+
+        @Test
+        public void get_0はhelloを返す() throws Exception {
+            assertThat(sut.get(0), is("hello"));
+        }
+
+        @Test
+        public void get_1はworldを返す() throws Exception {
+            assertThat(sut.get(1), is("world"));
+        }
     }
 
-    @Test(expected = IndexOutOfBoundsException.class)
-    public void リストが初期状態の時get_0はIndexOutOfBoundsExceptionを送出する() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.get(0);
-    }
-
-    @Test
-    public void リストに1件の文字列を含む時get_0はhelloを返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.add("hello");
-        assertThat(sut.get(0), is("hello"));
-    }
-
-    @Test
-    public void リストに2件の文字列を含む時get_0はhelloを返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.add("hello");
-        sut.add("world");
-        assertThat(sut.get(0), is("hello"));
-    }
-
-    @Test
-    public void リストに2件の文字列を含む時get_1はworldを返す() throws Exception {
-        ArrayList<String> sut = new ArrayList<>();
-        sut.add("hello");
-        sut.add("world");
-        assertThat(sut.get(1), is("world"));
-    }
 }
